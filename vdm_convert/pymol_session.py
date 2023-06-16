21533383770b8c36350cbfe8e4bbbbaa9dc99274
njf042 <nicholas.freitas@ucsf.edu>
2023-06-15 19:46:53 -0700

    fixed argument typo

--- vdm_convert/pymol_session.py
+++ vdm_convert/pymol_session.py
@@ -103,7 +103,7 @@ def main():
 	argp.add_argument('--input-dir', '-i', default = ".", help="Parquet input directory, relative to working directory.")
 	argp.add_argument('--input-type', '-t', default="PDB", help="Filetype of converted vdMs. Currently supports PDB, PQR, and XYZ.")
 	argp.add_argument('--score-cutoff', '-s', default=None, help="Minimum score to include. Default: include all.")
-	argp.add_argument('--residues', '-s', default=None, nargs='+', help="Residues to include. Default: include all.")
+	argp.add_argument('--residues', '-r', default=None, nargs='+', help="Residues to include. Default: include all.")
 	args = argp.parse_args()
 
 	create_session(args.input_dir, args.input_type, residues=args.residues, score_cutoff=args.score_cutoff)
