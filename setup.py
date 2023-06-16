a426022064a45f3421c29a4c1653417ddc254325
njf042 <nicholas.freitas@ucsf.edu>
2023-06-15 19:10:42 -0700

    Added required packages to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
+        'pymol',
+        'prody',
+        'numpy',
+        'pandas'
     ],
     entry_points={
         'console_scripts': [
