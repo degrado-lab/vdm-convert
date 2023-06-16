8f5a6fd51529a0010afce56240be8634f46aeeb5
njf042 <nicholas.freitas@ucsf.edu>
2023-06-15 17:16:46 -0700

    added modules to __init__.py

--- vdm-convert/__init__.py
+++ vdm-convert/__init__.py
@@ -4,4 +4,5 @@ This is the docstring for the example package.
 
 __version__ = '0.1'
 
-#from .example_module import example_function
+from .pymol_session import create_session
+from .parquet_convert import convert_parquet
